8957864d1630f50ab78018170edf032f9eb54739
Adrian Biedrzycki <a@modules.io>
2017-10-04 06:12:21 -0700

    Connection debugging

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const PORT = process.env['PORT'];
-
+const http = require('http');
+const express = require('express');
 const ws = require('ws');
-const server = ws.createServer({
-  port: PORT,
-});
+
+const PORT = process.env['PORT'] || 8000;
+
+const app = express();
+app.use(express.static(__dirname));
+const server = http.createServer(app);
+const wss = new ws.Server({server});
 const connections = [];
-wss.on('connection',c => {
-  c.on('message', m => {
-    c.send(m + ':response');
+wss.on('connection', (c, req) => {
+  const {url} = req;
+  console.log(url);
+
+  c.on('message', s => {
+    const m = JSON.parse(s);
+    const {url} = m;
+    const c2 = connections[url];
+    if (c2) {
+      const {message} = m;
+      c2.send(JSON.stringify({
+        type: 'message',
+        message,
+      }));
+      c.send(JSON.stringify({
+        type: 'response',
+        result: null,
+      }));
+    } else {
+      c.send(JSON.stringify({
+        type: 'response',
+        error: 'no such peer',
+      }));
+    }
   });
   c.on('close', m => {
-    connections.splice(connections.indexOf(c), 1);
+    connections[url] = null;
   });
 
-  connections.push(c);
+  connections[url] = c;
 });
+
+server.listen(PORT);
